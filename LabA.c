1757a92dded0141fd4a8e6ec7d8ebaf24362a877
bozhang-17 <159536035+bozhang-17@users.noreply.github.com>
2024-02-16 09:17:51 +0000

    Add files via upload

---
+++ LabA.c
@@ -0,0 +1,7 @@
+#include <stdio.h>//directive to include
+
+int main(void)  //function is run when the programs start
+{
+    printf("Hello. My email address is: \nbo.zhang-17@student.manchester.ac.uk\n");// our statement/command to run
+    return 0;//the return statement for the function main
+}
